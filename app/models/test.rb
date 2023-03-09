b7055399427b4bfc0b342bf1607aedaaa66f6e52
Evgeny Smirnov <e-smirnov@outlook.com>
2023-03-09 16:40:19 +0300

    feat: add Test: find_by_category method

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,7 @@
 class Test < ApplicationRecord
+  def self.find_by_category(name)
+    Test.joins("JOIN categories ON tests.category_id = categories.id")
+        .where("categories.title = ?", name)
+        .order(title: :desc)
+  end
 end
