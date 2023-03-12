685222f84d4046f1c8c433091c3b01c4372066cd
Evgeny Smirnov <e-smirnov@outlook.com>
2023-03-12 22:20:29 +0300

    feat: add ER-diagram, add models associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  has_many :created_tests, class_name: "Test", foreign_key: "author_id"
+  has_many :tests_users
+  has_many :tests, through: :tests_users
+
   def find_tests_by_level(test_level)
     Test.joins("JOIN users_tests ON tests.id = users_tests.test_id")
         .where(users_tests: { user_id: id }, tests: { level: test_level })
