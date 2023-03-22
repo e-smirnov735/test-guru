a87abd6923f70657ba298352767da6fb1e598950
Evgeny Smirnov <e-smirnov@outlook.com>
2023-03-22 22:46:54 +0300

    feat: routes and controller for questions

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,33 @@
+class QuestionsController < ApplicationController
+  before_action :current_question, only: %i[show destroy]
+
+  def index
+    @questions = Question.all
+  end
+
+  def new; end
+
+  def show; end
+
+  def create
+    Question.create!(question_params)
+    render plain: "вопрос успешно создан!"
+  end
+
+  def destroy
+    @question.destroy!
+    render plain: "вопрос: '#{@question.body}' из теста #{@question.test_id} успешно удален!"
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:body, :test_id)
+  end
+
+  def current_question
+    @question = Question.find(params[:id])
+  rescue => e
+    head :not_found
+  end
+end
