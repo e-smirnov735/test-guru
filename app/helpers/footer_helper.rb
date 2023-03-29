bbf58512ab2b11320bc1d07bf59bbac89e17d41d
Evgeny Smirnov <e-smirnov@outlook.com>
2023-03-29 23:38:31 +0300

    fix: delete link-method

--- app/helpers/footer_helper.rb
+++ app/helpers/footer_helper.rb
@@ -5,10 +5,6 @@ module FooterHelper
     Time.current.year
   end
 
-  def link(url, name)
-    link_to name, url, target: '_blank', rel: 'nofollow noopener'
-  end
-
   def github_url(author, repo)
     ["https://github.com", author, repo].join('/')
   end
