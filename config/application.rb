1977859f29e3a402e43a4ec32fd55beeb70b39a8
Evgeny Smirnov <e-smirnov@outlook.com>
2023-05-17 15:15:27 +0300

    feat: add localization

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module TestGuru
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-    config.i18n.default_locale = :en
+    config.i18n.default_locale = :ru
     config.time_zone = 'Moscow'
 
     # Configuration for the application, engines, and railties goes here.
