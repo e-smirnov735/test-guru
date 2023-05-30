df15af3c45a9f8eab9962b0b49da822586a55b81
Evgeny Smirnov <e-smirnov@outlook.com>
2023-05-30 15:21:13 +0300

    feat: add sort by title, add passwords check

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import './alert'
+import './utilities/sorting'
+import './utilities/passwords_comparison'
 
 Rails.start()
 Turbolinks.start()
---
+++ app/javascript/packs/utilities/passwords_comparison.js
@@ -0,0 +1,28 @@
+document.addEventListener("turbolinks:load", function () {
+  document
+    .querySelectorAll(".reg-form .form-control[type=password]")
+    .forEach((el) => el.addEventListener("input", passwordsCompare));
+});
+
+function passwordsCompare() {
+  const passValue = document.querySelector(
+    ".form-control[id=user_password]"
+  ).value;
+  const confirmValue = document.querySelector(
+    ".form-control[id=user_password_confirmation]"
+  ).value;
+  const icon = document.querySelector(".reg-form .octicon-alert");
+
+  if (confirmValue) {
+    if (passValue != confirmValue) {
+      icon.classList.remove("octicon-alert__correct", "hide");
+      icon.classList.add("octicon-alert__wrong");
+    } else {
+      icon.classList.remove("octicon-alert__wrong");
+      icon.classList.add("octicon-alert__correct");
+    }
+  } else {
+    icon.classList.remove("octicon-alert__correct", "octicon-alert__wrong");
+    icon.classList.add("hide");
+  }
+}
