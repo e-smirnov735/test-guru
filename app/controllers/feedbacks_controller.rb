fae9308086ef88b58d9dc0c6d03b1879866abfd7
Evgeny Smirnov <e-smirnov@outlook.com>
2023-07-26 16:47:42 +0300

    feat: add feedback

---
+++ app/controllers/feedbacks_controller.rb
@@ -0,0 +1,23 @@
+class FeedbacksController < ApplicationController
+  def new
+    @feedback = Feedback.new
+  end
+
+  def create
+    @feedback = Feedback.new(feedback_params)
+
+    if @feedback.save
+      flash[:success] = 'Сообщение успешно отправлено'
+      redirect_to root_path
+    else
+      flash[:alert] = 'Произошла ошибка'
+      render :new
+    end
+  end
+
+  private
+
+  def feedback_params
+    params.require(:feedback).permit(:name, :email, :message)
+  end
+end
