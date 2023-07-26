de64bf0acc5eb992f23dde656202cd92fd508bb6
Evgeny Smirnov <e-smirnov@outlook.com>
2023-07-27 00:06:14 +0300

    fix: smtp v2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.smtp_setting = {
+  config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
     user_name: ENV.fetch('SMTP_USERNAME', nil),
