ec2cf314439e8890da28f72c95022d8d2e91d566
Evgeny Smirnov <e-smirnov@outlook.com>
2023-07-31 22:38:28 +0300

    feat: prepare db, create badge controller

--- db/migrate/20230731101913_create_badges.rb
+++ db/migrate/20230731101913_create_badges.rb
@@ -2,8 +2,15 @@ class CreateBadges < ActiveRecord::Migration[6.1]
   def change
     create_table :badges do |t|
       t.string :title, null: false
-      t.string :type, null: false
+      t.string :rule, null: false
       t.string :url, null: false
+
+      t.timestamps
+    end
+
+    create_table :user_badges do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :badge, null: false, foreign_key: true
 
       t.timestamps
     end
