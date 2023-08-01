a83f72ad681fc6382780ec2376ec29af6751908a
Evgeny Smirnov <e-smirnov@outlook.com>
2023-08-01 23:27:21 +0300

    feat: crud for badge

--- app/controllers/admin/badges_controller.rb
+++ app/controllers/admin/badges_controller.rb
@@ -1,5 +1,49 @@
 class Admin::BadgesController < Admin::BaseController
+  before_action :find_badge, only: %i[show edit update destroy]
+
   def index
     @badges = Badge.all
+  end
+
+  def new
+    @badge = Badge.new
+  end
+
+  def create
+    @badge = Badge.new(badge_params)
+
+    if @badge.save
+      redirect_to admin_badges_path
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def show; end
+
+  def update
+    if @badge.update(badge_params)
+      redirect_to admin_badge_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @badge.destroy!
+
+    redirect_to admin_badge_path
+  end
+
+  private
+
+  def find_badge
+    @badge = Badge.find(params[:id])
+  end
+
+  def badge_params
+    params.require(:badge).permit(:title, :rule, :url)
   end
 end
