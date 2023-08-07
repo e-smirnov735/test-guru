e23319c788351e34ca46698847085aff4b9a8736
Evgeny Smirnov <e-smirnov@outlook.com>
2023-08-08 00:23:54 +0300

    feat: add list of image and rule when create badge

--- app/helpers/admin/badge_helper.rb
+++ app/helpers/admin/badge_helper.rb
@@ -1,2 +1,9 @@
 module Admin::BadgeHelper
+  def image_list
+    Dir['app/assets/images/*'].map { |path| File.basename(path) }
+  end
+
+  def rule_list
+    image_list.map { |path| path.delete_suffix('.png') }
+  end
 end
